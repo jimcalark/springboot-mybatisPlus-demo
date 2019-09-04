364c4b8897d02d7ff7b9e8629f658480e2eab139
Jim Clark <571009744@qq.com>
2019-09-04 17:55:04 +0800

    单表操作2

--- src/main/java/com/train/mp/controller/UserController.java
+++ src/main/java/com/train/mp/controller/UserController.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
